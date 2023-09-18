aa1bf5009f2b862216a213d204c04ac6c7c9b466
aditya993388 <adityapratapsinghtomer@gamil.com>
2023-09-18 13:51:05 +1000

    uploading

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <tennis />
+  </div>
+</template>
+
+<script>
+import tennis from "./components/tennis.vue";
+
+export default {
+  components: {
+    tennis,
+  },
+};
+</script>
---
+++ src/components/tennis.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>{{ pageTitle }}</h1>
+      <div v-html="htmlContent"></div>
+      <img :src="imageURL" :alt="imageAlt" />
+      <p>{{ message }}</p>
+      <button @click="showMessage">Show Message</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const pageTitle = ref("All about Tennis");
+  const htmlContent = ref("<p>We will learn the basics of Tennis</p>");
+  const imageURL = ref("C:\\Users\\rayfa\\OneDrive\\Pictures\\Saved Pictures\\tennis_image.jpeg");
+  const imageAlt = ref("Cool Tennis Image");
+  const message = ref("");
+  
+  const showMessage = () => {
+    message.value = "Are you excited to learn about tennis?";
+  };
+  </script>
+  
